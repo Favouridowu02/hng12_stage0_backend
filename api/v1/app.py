a64e5c4cbbfc214e620d3f6632c629573845061f
Favour Idowu <favouridowu02@gmail.com>
2025-01-30 18:41:14 +0100

    fix: import the loadenv and Restructure the code

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,16 +8,11 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from os import getenv
 
+load_dotenv()
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r'/api/v1/*': {'origins': "*"}})
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(404)
@@ -39,4 +34,4 @@ if __name__ == "__main__":
     """
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
